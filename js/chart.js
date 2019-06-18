d5aa0290cb642497fd2992c95cdc36e890587a9b
mustakrakish <mustakrkish@gmail.com>
2019-06-18 13:44:16 +0300

    v.2

--- js/chart.js
+++ js/chart.js
@@ -8,19 +8,9 @@ $(document).ready(function () {
     var activeChannels = displays[activeDisplay].channels;
     selectChannels(activeChannels);
     var channelNames = getChannelNames(channels, activeChannels);
-
-    /*var timeB = new Date();
     var channelData = getChannelData(activeChannels, date);
     buildGraph(channelData, channelNames);
     var time = new Date() - timeB;
-    alert ('Время операции: ' + time + ' мс');*/
-
-
-    var timeB = new Date();
-    var channelData1 = getChannelData1(activeChannels, date);
-    buildGraph1(channelData1, channelNames);
-    var time = new Date() - timeB;
-    alert('Время операции: ' + time + ' мс');
 
     $('form').submit(function (event) {
         event.preventDefault();
@@ -44,13 +34,9 @@ $(document).ready(function () {
         activeDisplay = $('[name=display] option:selected').val();
         activeChannels = displays[activeDisplay].channels;
         selectChannels(activeChannels);
-
-        var timeB = new Date();
         channelData = getChannelData1(activeChannels, date);
         channelNames = getChannelNames(channels, activeChannels);
-        buildGraph1(channelData, channelNames);
-        var time = new Date() - timeB;
-        alert('Время операции: ' + time + ' мс');
+        buildGraph(channelData, channelNames);
     });
 
     $('[name=channels]').change(function (event) {
@@ -185,40 +171,6 @@ function getChannelNames(channels, activeChannels) {
 }
 
 function buildGraph(graphData, channelNames) {
-    $('#chart').empty();
-    var chart = anychart.stock();
-    chart.container('chart');
-    var plot = chart.plot();
-
-    chart.xScale().minimumGap({
-        intervalsCount: 365,
-        unitType: 'day',
-        unitCount: 1
-    });
-
-    chart.xScale().maximumGap({
-        intervalsCount: 730,
-        unitType: 'day',
-        unitCount: 1
-    });
-
-    chart.xScale('scatter');
-
-    plot.removeAllSeries();
-
-    graphData.forEach(function (channel, channelNum, channels) {
-        var dataTable = anychart.data.table(0, 0, 2);
-        dataTable.addData(channels[channelNum]);
-        plot.line(dataTable.mapAs({ value: 1 })).name(channelNames[channelNum]);
-    })
-
-    plot.legend().position('bottom');
-    plot.legend().itemsLayout("horizontal-expandable");
-
-    chart.draw();
-}
-
-function buildGraph1(graphData, channelNames) {
     $('#chart').empty();
     var chart = anychart.stock();
     chart.container('chart');
--- php/chart/functions.php
+++ php/chart/functions.php
@@ -95,41 +95,6 @@
         $fileHandler = fopen($path, 'rb');
         $file_channelCount = unpack('S*', fread($fileHandler, $SIZE_CHANNEL_COUNT), 0)[1];
         $size_serviceData = $SIZE_CHANNEL_COUNT+$SIZE_DATE+$SIZE_CONF*$file_channelCount;
-        $channelPointsCount = (filesize($path)-$size_serviceData)/($SIZE_VALUE*$file_channelCount)-1;
-        $interval = 86400/$channelPointsCount;
-        $shift = $SIZE_VALUE*$file_channelCount;
-
-        fseek($fileHandler, $size_serviceData);
-        foreach($channels as $channel){
-            $channelPoints = [];
-            for ($i=0; $i<$channelPointsCount; $i++){
-                $timestamp = (strtotime(date('Y-m-d', filemtime($path)-1).' 00:00:00') + $interval*$i)*1000;
-                
-                //$time = gmdate('Y-m-d H:i:s',  $interval*$i);
-                fseek($fileHandler, $size_serviceData + $shift * $i + $SIZE_VALUE * $channel);
-                $value = round(unpack('f*', fread($fileHandler, $SIZE_VALUE), SEEK_SET)[1], 3);
-                array_push($channelPoints, [$timestamp, $value]);
-            }
-            array_push($channelData, $channelPoints);
-        }
-            
-        fclose($fileHandler);
-        return $channelData;
-    }
-
-    function getChannelData1($path, $channels){
-        $channelData = [];
-
-        $SIZE_CHANNEL_COUNT = 2;
-        $SIZE_DATE = 16;
-        $SIZE_CONF = 61;
-        $SIZE_VALUE = 4;
-        
-        $date = str_split(basename($path, ".arh"), 2);
-        filemtime($path);
-        $fileHandler = fopen($path, 'rb');
-        $file_channelCount = unpack('S*', fread($fileHandler, $SIZE_CHANNEL_COUNT), 0)[1];
-        $size_serviceData = $SIZE_CHANNEL_COUNT+$SIZE_DATE+$SIZE_CONF*$file_channelCount;
         $pointCount = (filesize($path)-$size_serviceData)/($SIZE_VALUE*$file_channelCount)-1;
         $interval = 86400/$pointCount;
         $shift = $SIZE_VALUE*$file_channelCount;
@@ -138,7 +103,7 @@
 
         fseek($fileHandler, $size_serviceData);
             for ($pointNum=0; $pointNum<$pointCount; $pointNum++){
-                $point = [$timeB + $interval * $pointNum];
+                $point = [($timeB + $interval * $pointNum) * 1000];
                 foreach($channels as $key => $channel){
                     fseek($fileHandler, $size_serviceData + $shift * $pointNum + $SIZE_VALUE * $channel);
                     $value = round(unpack('f*', fread($fileHandler, $SIZE_VALUE), SEEK_SET)[1], 3);
