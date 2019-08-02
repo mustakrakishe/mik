92cee23bb672d60a662b7785b017a5e243e54a18
mustakrakishe <mustakrkish@gmail.com>
2019-08-02 15:39:28 +0300

    Открытая боковая панель с крестиком. Не сворачивается.

--- chart.php
+++ chart.php
@@ -1,5 +1,6 @@
 <?php require 'php/common/head.php'; ?>
 <link rel="stylesheet" href="css/chart.css">
+<link rel="stylesheet" href="css/font-awesome-4.7.0/css/font-awesome.min.css">
 <script src='js/jquery-3.3.1.min.js'></script>
 <script src='js/chart.js'></script>
 
@@ -8,20 +9,23 @@
 <script src='js/anychart/anychart-exports.min.js'></script>
 <script src='js/anychart/anychart-base.min.js'></script>
 
-<div id="controlRow">
-    <form action="" method="post">
-        <input type="date" name="date" value="2018-05-11">
-        <select name="display"></select>
-        <input type="submit" name="build_btn" hidden="hidden">
-    </form>
+<div id="mainContent-wrap" class="content-wrap">
+    <div id="controlRow" class="content-header">
+        <form action="" method="post">
+            <input type="date" name="date" value="2018-05-11">
+            <select id="display" name="display"></select>
+            <input type="submit" name="build_btn" hidden="hidden">
+        </form>
+    </div>
+
+    <div id="chartRow" class="content">
+        <div id="chart"></div>
+    </div>
 </div>
 
-<div id="chartRow">
-    <div id="chart"></div>
-    <div id="tab-channels">
-        <div id="tab-channels-tongue"></div>
-        <select id="tab-channels-list" multiple id="tab-channels-list" name="channels"></select>
-    </div>
+<div id="tab-channels" class="content-wrap tab">
+    <div id="tab-channels-header" class="content-header">Каналы <i class="fa fa-times icon-close-window" aria-hidden="true"></i></div>
+    <select id="tab-channels-list" class="content" multiple id="tab-channels-list" name="channels"></select>
 </div>
 
 <?php require 'php/common/foot.php'; ?>
