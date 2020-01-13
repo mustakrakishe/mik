eda60ce951c6382d9145017b2f7997da509a9aa5
mustakrakishe <mustakrkish@gmail.com>
2020-01-13 10:04:49 +0200

    Исправлен php/getChannels, где path задавался строкой, а не из GET

--- php/chart/getChannels.php
+++ php/chart/getChannels.php
@@ -1,7 +1,6 @@
 <?php
     $path = $_GET['path'];
     require 'functions.php';
-    $path = 'D:/documents/task/task2018_web_interface/page/current/data/O11/2018-05-11/chanel.bas';
 
     echo json_encode(getChannels($path));
 ?>
